d2a65ba46be028822354b230c0e551a547757df0
ngovudan <danngo2000@gmail.com>
2021-04-25 09:14:01 +0700

    commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import './App.css';
+import Login from "./components/Login";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import './Login.css'
+
+
+const Login = () => {
+    return (
+        <section className="container">
+            <div className="content">
+                <div className="BgImage"></div>
+            </div>
+        </section>
+    )
+}
+
+export default Login
